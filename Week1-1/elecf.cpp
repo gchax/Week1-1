ae8a91fe6ba2528c0673e800689c7dd87e24931a
Chanon <pcg.mega@gmail.com>
2020-08-14 00:32:01 +0700

    nowarning

--- Week1-1/elecf.cpp
+++ Week1-1/elecf.cpp
@@ -1,14 +1,15 @@
 #include<stdio.h>
 #include<cmath>
+#define _CRT_SECURE_NO_WARNINGS
 #define k 9e9
 
 int main() {
 	double f, Q, q, r;
 	printf("Electrical Force Calculator (If unknown, put in -0.)\n");
-	printf("Electrical Force (F) = "); scanf_s("%lf", &f);
-	printf("Charge #1 (Q) = "); scanf_s("%lf", &Q);
-	printf("Charge #2 (q) = "); scanf_s("%lf", &q);
-	printf("Distance (r) = "); scanf_s("%lf", &r);
+	printf("Electrical Force (F) = "); scanf("%lf", &f);
+	printf("Charge #1 (Q) = "); scanf("%lf", &Q);
+	printf("Charge #2 (q) = "); scanf("%lf", &q);
+	printf("Distance (r) = "); scanf("%lf", &r);
 
 	q = abs(q);
 	Q = abs(Q);
